6abbf451b173e13a0744cdc84f0a6bc0dbb5a108
andreteixeiraUFS <dezaoteixeira@gmail.com>
2017-03-13 11:51:13 -0300

    Mudança de projeto no topo da pagina - melhorias em geral

--- adm/adm.controller.js
+++ adm/adm.controller.js
@@ -5,32 +5,55 @@
         .module('app')
         .controller('AdmController', AdmController);
 
-    AdmController.$inject = ['AccountService', '$rootScope', '$cookieStore'];
-    function AdmController(AdmService, $rootScope, $cookieStore) {
+    AdmController.$inject = ['AccountService', 'ProjectService', '$rootScope', '$cookieStore','$state'];
+    function AdmController(AdmService,ProjectService, $rootScope, $cookieStore, $state) {
         var vm = this;
 
         vm.user = {};
         vm.project = {};
-        vm.year ={};
+        vm.projects = [];
+        vm.year = {};
+
+        vm.openProject = openProject;
+        vm.getAllProjects = getAllProjects;
 
         initController();
 
         function initController() {
             loadCurrentUser();
             date();
+            getAllProjects();
         }
 
-function date() {
-    var date = new Date();
-    vm.year = date.getFullYear();
-   }
+        function date() {
+            var date = new Date();
+            vm.year = date.getFullYear();
+        }
 
 
         function loadCurrentUser() {
-          vm.user.dsName = $rootScope.globals.currentUser.dsName;
-          vm.user.dsUsername = $rootScope.globals.currentUser.dsUsername;
-          vm.user.dsEmail = $rootScope.globals.currentUser.dsEmail;
-          vm.project = $cookieStore.get("currentProject");
+            vm.user.dsName = $rootScope.globals.currentUser.dsName;
+            vm.user.dsUsername = $rootScope.globals.currentUser.dsUsername;
+            vm.user.dsEmail = $rootScope.globals.currentUser.dsEmail;
+            vm.project = $cookieStore.get("currentProject");
+        }
+
+        function getAllProjects() {
+            vm.dataLoading = true;
+            var dsSso = $rootScope.globals.currentUser.dsUsername;
+            ProjectService.GetAllByDsSsoResearcher(dsSso).then(function (response) {
+                var projects = response;
+                vm.projects = projects;
+                vm.dataLoading = false;
+            });
+        }
+
+        function openProject() {
+            ProjectService.GetByDsKey(vm.project.dsKey).then(function (response) {
+                var project = response;
+                $cookieStore.put('currentProject', project);
+                $state.go($state.current, {}, { reload: true });
+            });
         }
     }
 })();
